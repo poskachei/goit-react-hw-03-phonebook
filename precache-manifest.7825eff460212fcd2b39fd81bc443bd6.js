abfc4abf82aca80a7c6ecdaba18ac893a1e3072a
poskachei <vposkachey@gmail.com>
2020-09-13 11:58:19 +0300

    Updates

---
+++ precache-manifest.7825eff460212fcd2b39fd81bc443bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2aa026058863b809f1165218938f709",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "11948fa193a09277f956",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.927d6e72.chunk.css"
+  },
+  {
+    "revision": "d83fa12c9cb461f9500b",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.11922e35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.11922e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11948fa193a09277f956",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.4d193ead.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
